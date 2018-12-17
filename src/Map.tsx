3f6a6848fa50dbc0131822bbf14b2935ad0a7a7d
Gaëtan Rizio <gaetan+git@rizio.fr>
2018-12-17 20:54:21 +0100

    Add some evidence on the moving seat
    When moving a seat, we do not see which one is moving.
    To avoid confusion, we set different styles between seats and the moving seat.
    
    This commit does the following:
    
    - Set the default style of a seat as white with black border
    - When the seat is moving set it as grey with black border

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,6 +1,6 @@
 import {h} from "preact"
 import {Shadow} from "./Shadow";
-import {State} from "./State";
+import {isMovingSeat, State} from "./State"
 import {Seat} from "./models/Seat"
 
 interface Props {
@@ -9,21 +9,27 @@ interface Props {
   confirmAction: () => void
 }
 
-export function Map({state, startMoveSeat, confirmAction}: Props) {
+export function Map(props: Props) {
+  const {state, confirmAction} = props
   return (
     <svg class="map">
       {
-        state.seats.map(seat => renderSeat(seat, startMoveSeat))
+        state.seats.map(seat => renderSeat(seat, props))
       }
       <Shadow state={state} confirmAction={confirmAction}/>
     </svg>
   )
 }
 
-function renderSeat(seat: Seat, startMoveSeat: (id: number) => void) {
+function renderSeat(seat: Seat, props: Props) {
+  const action = props.state.action
+  const movingSeatId = action !== undefined && isMovingSeat(action) ? action.seat.id : undefined
   return (
-    <g id={`seat-${seat.id}`} onClick={() => startMoveSeat(seat.id)}>
-      <rect x={seat.x} y={seat.y} width={50} height={50}/>
+    <g id={`seat-${seat.id}`} onClick={() => props.startMoveSeat(seat.id)}>
+      <rect x={seat.x} y={seat.y} width={50} height={50}
+            fill={seat.id === movingSeatId ? "#ccc" : "#fff"}
+            stroke="black"
+      />
     </g>
   )
 }
