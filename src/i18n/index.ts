527c679287ec20bd7828b25dd6880d794b31eacd
Gaëtan Rizio <gaetan+git@rizio.fr>
2018-12-29 20:45:22 +0100

    Remove unwanted console.log

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -5,9 +5,6 @@ import { I18N } from 'i18n/I18N'
 export type Language = "en" | "fr"
 
 export default function i18n(key: keyof I18N, language?: Language): string {
-  if (!language) {
-    console.log(key, language)
-  }
   switch (language) {
     case "en":
       return en[key]
